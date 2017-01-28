194cc65ca2faa120aeda6d4e42054d5a7941f2cd
Federico Gandellini <fgandellini@gmail.com>
2017-01-28 10:06:12 +0100

    apply some refactoring

--- app/index.js
+++ app/index.js
@@ -1,76 +1,50 @@
-const MAX_X = 5
-const MAX_Y = 5
+const PLANET = [
+  ['.','.','.','.','.'],
+  ['.','.','.','.','.'],
+  ['.','.','^','.','.'],
+  ['.','^','.','.','.'],
+  ['.','.','.','.','.'],
+]
+
+const MAX_X = PLANET[0].length
+const MAX_Y = PLANET.length
 
 const wrapX = x => Math.abs(x + MAX_X) % MAX_X
 const wrapY = y => Math.abs(y + MAX_Y) % MAX_Y
 
-const OBSTACLES = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-]
+const leftTurns = { N: 'W', S: 'E', W: 'S', E: 'N' }
+const rightTurns = { N: 'E', S: 'W', W: 'N', E: 'S' }
 
-const tryMove = (startX, startY, endX, endY) =>
-  !OBSTACLES[endY][endX] ?
+const calcPosition = (startX, startY, endX, endY) =>
+  (PLANET[endY][endX] === '.') ?
     ({ x: endX, y: endY, s: 'ok' }) :
     ({ x: startX, y: startY, s: 'OBSTACLE_FOUND' })
 
-const moveForward = ({ x, y, d, s }) => {
-  switch (d) {
-    case 'N': return Object.assign(tryMove(x, y, x, wrapY(y-1)), { d })
-    case 'S': return Object.assign(tryMove(x, y, x, wrapY(y+1)), { d })
-    case 'W': return Object.assign(tryMove(x, y, wrapX(x-1), y), { d })
-    case 'E': return Object.assign(tryMove(x, y, wrapX(x+1), y), { d })
-    default: return ({ x, y, d, s })
-  }
-}
+const forwardMovements = (x, y) => ({
+  N: calcPosition(x, y, x, wrapY(y-1)),
+  S: calcPosition(x, y, x, wrapY(y+1)),
+  W: calcPosition(x, y, wrapX(x-1), y),
+  E: calcPosition(x, y, wrapX(x+1), y),
+})
 
-const moveBackward = ({ x, y, d, s }) => {
-  switch (d) {
-    case 'N': return Object.assign(tryMove(x, y, x, wrapY(y+1)), { d })
-    case 'S': return Object.assign(tryMove(x, y, x, wrapY(y-1)), { d })
-    case 'W': return Object.assign(tryMove(x, y, wrapX(x+1), y), { d })
-    case 'E': return Object.assign(tryMove(x, y, wrapX(x-1), y), { d })
-    default: return ({ x, y, d, s })
-  }
-}
+const backwardMovements = (x, y) => ({
+  N: calcPosition(x, y, x, wrapY(y+1)),
+  S: calcPosition(x, y, x, wrapY(y-1)),
+  W: calcPosition(x, y, wrapX(x+1), y),
+  E: calcPosition(x, y, wrapX(x-1), y),
+})
 
-const turnLeft = ({ x, y, d, s}) => {
-  switch (d) {
-    case 'N': return ({ x, y, d: 'W', s })
-    case 'S': return ({ x, y, d: 'E', s })
-    case 'W': return ({ x, y, d: 'S', s })
-    case 'E': return ({ x, y, d: 'N', s })
-    default: return d
-  }
-}
+const movements = ({ x, y, d, s }) => ({
+  f: Object.assign({ s, d }, forwardMovements(x, y)[d]),
+  b: Object.assign({ s, d }, backwardMovements(x, y)[d]),
+  l: Object.assign({ s, x, y }, { d: leftTurns[d] }),
+  r: Object.assign({ s, x, y }, { d: rightTurns[d] }),
+})
 
-const turnRight = ({ x, y, d, s}) => {
-  switch (d) {
-    case 'N': return ({ x, y, d: 'E', s})
-    case 'S': return ({ x, y, d: 'W', s})
-    case 'W': return ({ x, y, d: 'N', s})
-    case 'E': return ({ x, y, d: 'S', s})
-    default: return d
-  }
-}
-
-const move = (state, command) => {
-  switch (command) {
-    case 'f': return moveForward(state)
-    case 'b': return moveBackward(state)
-    case 'l': return turnLeft(state)
-    case 'r': return turnRight(state)
-    default: return state
-  }
-}
-
-const mr = ({ x, y, d, s }, commands) =>
+const mr = (state, commands) =>
   (commands || []).reduce(
-    (state, command) => move(state, command),
-    { x, y, d, s: 'ok' }
+    (state, command) => movements(state)[command],
+    Object.assign({}, state, { s: 'ok' })
   )
 
 module.exports = mr
